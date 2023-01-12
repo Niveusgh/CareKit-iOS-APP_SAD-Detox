890fb72dcf6a3e02b0f277c8cbad3f0921fda617
Thea Francis <mengtinghe@outlook.com>
2023-01-12 13:37:12 -0500

    Initial Commit

---
+++ SAD Detox/SAD_DetoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAD_DetoxApp.swift
+//  SAD Detox
+//
+//  Created by Thea He on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SAD_DetoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
